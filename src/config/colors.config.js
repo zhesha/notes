be67444e69bd2f299402562cd8da054f9ef6156b
zhesha <savzhe@gmail.com>
2019-10-30 18:41:12 +0200

    updated colors for better contrast

--- src/config/colors.config.js
+++ src/config/colors.config.js
@@ -1,20 +1,20 @@
 const list = [
-  '888888',
-  'BE8F32',
-  'BE3934',
-  'BE2D85',
-  'AE35BE',
-  '6834BE',
-  '3552BE',
-  '3699BE'
+  'A64D79',
+  'A61C00',
+  'CC0000',
+  'E69438',
+  'F1C232',
+  '3C79D8',
+  '3D86C6',
+  '674EA7'
 ];
 
 const colors = {
-  newNote: '90ee90',
-  newContrast: '888',
-  noteContrast: '444',
+  newNote: '6AA84F',
+  newContrast: 'F3F3F3',
+  noteContrast: 'F3F3F3',
   gravatarContrast: 'fff',
-  variantContrast: '9bbe92',
+  variantContrast: 'F3F3F3',
   list: list,
   default: list[0]
 };
